e99b7d8d9568865500243427f3ea6a82dd0e47fe
Arisu-Liddell <cafe9679saito@email.com>
2025-11-06 17:06:03 +0900

    Assimpライブラリの機能拡張と新規ファイルの追加
    Assimpライブラリにおいて、モデルの読み込み、描画、解放機能を追加し、DirectXバッファへの変換をサポートしました。また、PBRマテリアル、ポストプロセスステップ、メタデータ管理、ベクトル・行列演算、クォータニオン操作など、多数の新機能を追加しました。
    
    - `matrix4x4.inl` などで行列操作のインライン実装を追加。
    - `mesh.h` でメッシュデータ構造を定義。
    - `metadata.h` でメタデータ管理機能を追加。
    - `pbrmaterial.h` でPBRマテリアル関連のマクロを追加。
    - Android向けのIOシステム (`AndroidJNIIOSystem.h`, `BundledAssetIOSystem.h`) を追加。
    - `postprocess.h` でポストプロセスステップを定義。
    - `quaternion.h` でクォータニオン操作をサポート。
    - `scene.h` でシーンデータ構造を追加。
    - `model.cpp` でモデルの読み込み、描画、解放機能を実装。
    - `shaderPixel3D.hlsl`, `shaderVertex3D.hlsl` でシェーダーにライティング計算を追加。
    - その他、多数のヘッダーファイルを追加し、ライブラリ全体の拡張性と使いやすさを向上。

--- shaderPixel3D.hlsl
+++ shaderPixel3D.hlsl
@@ -2,9 +2,10 @@ Texture2D tex : register(t0);
 SamplerState samplerState : register(s0);
 
 float4 main(in float4 position : SV_Position,
-            in float2 texcoord : TEXCOORD0) : SV_TARGET
+            in float2 texcoord : TEXCOORD0,
+            in float4 color : COLOR0) : SV_TARGET
 {
-    return tex.Sample(samplerState, texcoord);
+    return tex.Sample(samplerState, texcoord) * color;
 }
 
 // return float4(255.0f, 75.0f, 0.0f, 1.0f); 
--- shaderVertex3D.hlsl
+++ shaderVertex3D.hlsl
@@ -5,8 +5,22 @@ float4x4 mtx;
 float4 main(in float4 position : POSITION0,//セマンティック
             in float2 texcoord : TEXCOORD0,
             in float3 normal   : NORMAL0,
-            out float2 outTexcoord : TEXCOORD0) : SV_Position
+            out float2 outTexcoord : TEXCOORD0,
+            out float4 outColor : COROR0) : SV_Position
 {
+    //ライト方向ベクトル
+    float lightDirection = float3(0.0, -1.0, 0.0);
+    //ランバート拡散照明
+    outColor.rgb = saturate( - dot(lightDirection, normal));
+    outColor.a = 1.0;
+    
     outTexcoord = texcoord;
     return mul(position, mtx);
 }
+
+
+
+//ランバート拡散照明7
+// 
+//明るさ = - L * N
+//             内積 = (cosΘ)
